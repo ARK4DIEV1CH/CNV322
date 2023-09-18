60fa18e65a6f9763ddc28e6ee94ae3b4647ff1af
Misha <mishany_slesh@mail.ru>
2023-09-18 11:56:49 +0300

    cool update

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   root "home#index"
   get "step2", to: "home#step2"
+  post "step2", to: "home#step2"
   get "step3", to: "home#step3"
+  post "step3", to: "home#step3"
   get "step4", to: "home#step4"
+  post "step4", to: "home#step4"
 end
